51355ca21e3a9beadaedf66ea6aa165688cb8384
KlajdixGashi <115544942+KlajdiGashi@users.noreply.github.com>
2023-04-16 22:46:31 +0200

    Updated code

--- src/Reciever/Reciever.java
+++ src/Reciever/Reciever.java
@@ -4,9 +4,9 @@ class Reciever extends Person {
     public String plainTextIfCompleteWord;
     public String plainTextIfSingleLetter;
 
-    public Receiver(ArrayList<String> encryptedTextWithCompleteWord, ArrayList<String> encryptedTextWithSingleLetter) 
-    throws IOException {
-    super();
+    public Receiver(ArrayList<String> encryptedTextWithCompleteWord, ArrayList<String> encryptedTextWithSingleLetter,String bookUrl) 
+     throws IOException {
+    super(bookUrl);
     this.plainTextIfCompleteWord = this.generatePlainTextIfCompleteWord(this.createArrayOutOfKey(encryptedTextWithCompleteWord), this.bookAsArrayOfRows);
     this.plainTextIfSingleLetter = this.generatePlaintextIfSingleLetter(this.createArrayOutOfKey(encryptedTextWithSingleLetter), this.bookAsArrayOfRows);
     System.out.println("\n"+"Hello Sender! I was able to crack your code and this is what i found out: " + "\n" + this.plainTextIfCompleteWord);
