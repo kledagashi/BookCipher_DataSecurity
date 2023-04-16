6bd1a22a3b11f71adadb98f8e9f34272c3a20670
Gentrit851 <53408143+Gentrit851@users.noreply.github.com>
2023-04-16 21:37:14 +0200

    Update Main.java
    Applied changes made in other classes in this class too.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) throws IOException {
-        Sender sender = new Sender("I have never been here before");
-        Receiver receiver = new Receiver(sender.myEncryptedTextWithCompleteWords,sender.myEncryptedTextWithSingleLetter);
+        String bookURL = "C:\\Users\\Admin\\IdeaProjects\\DataSec_Project02\\out\\production\\DataSec_Project02\\Crime and Punishment.txt";
+        Sender sender = new Sender("I have never been here before", bookURL);
+        Receiver receiver = new Receiver(sender.myEncryptedTextWithCompleteWords, sender.myEncryptedTextWithSingleLetter, bookURL);
     }
 }
