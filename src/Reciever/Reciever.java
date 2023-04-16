21963fabdc0a6a44f9ceced8ce2b68dc02a2e6bc
KlajdiGashi <klajdi.gashi@student.uni-pr.edu>
2023-04-16 17:56:13 +0200

    added the code for the constructor

--- src/Reciever/Reciever.java
+++ src/Reciever/Reciever.java
@@ -6,5 +6,12 @@ class Reciever extends Person {
 
     public Receiver(ArrayList<String> encryptedTextWithCompleteWord, ArrayList<String> encryptedTextWithSingleLetter) 
     throws IOException {
+    super();
+    this.plainTextIfCompleteWord = this.generatePlainTextIfCompleteWord(this.createArrayOutOfKey(encryptedTextWithCompleteWord), this.bookAsArrayOfRows);
+    this.plainTextIfSingleLetter = this.generatePlaintextIfSingleLetter(this.createArrayOutOfKey(encryptedTextWithSingleLetter), this.bookAsArrayOfRows);
+    System.out.println("\n"+"Hello Sender! I was able to crack your code and this is what i found out: " + "\n" + this.plainTextIfCompleteWord);
+    System.out.println(this.plainTextIfSingleLetter);
+
+     
     }
 }
