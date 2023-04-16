c1af15b974740d1e02911db0c21f6d6adbbbc68d
Gentrit851 <53408143+Gentrit851@users.noreply.github.com>
2023-04-16 22:50:27 +0200

    Update generateEncryptedTextWithCompleteWord.java
    Update on error handling.

--- src/Sender/generateEncryptedTextWithCompleteWord.java
+++ src/Sender/generateEncryptedTextWithCompleteWord.java
@@ -1,23 +1,28 @@
-public ArrayList<String> generateEncryptedTextWithCompleteWord(ArrayList<String> plaintextAsArrayOfWords, ArrayList<ArrayList<String>> bookAsArrayOfRows) throws RuntimeException {
+    public ArrayList<String> generateEncryptedTextWithCompleteWord(ArrayList<String> plaintextAsArrayOfWords, ArrayList<ArrayList<String>> bookAsArrayOfRows) throws RuntimeException {
         ArrayList<String> encryptedTextWithCompleteWord = new ArrayList<>();
+        try {
             int numberOfWordsThatHaveBeenFound = 0;
             for (int i = 0; i < bookAsArrayOfRows.size(); i++) {
                 for (int j = 0; j < bookAsArrayOfRows.get(i).size(); j++) {
                     if (numberOfWordsThatHaveBeenFound < plaintextAsArrayOfWords.size()) {
                         if (bookAsArrayOfRows.get(i).get(j).equalsIgnoreCase(plaintextAsArrayOfWords.get(numberOfWordsThatHaveBeenFound))) {
-                            encryptedTextWithCompleteWord.add((i + 1) + "/" + (j + 1) );
+                            encryptedTextWithCompleteWord.add((i + 1) + "/" + (j + 1));
                             numberOfWordsThatHaveBeenFound++;
                             i = 0;
                             j = 0;
                         }
-                    }
-                    else if (numberOfWordsThatHaveBeenFound == plaintextAsArrayOfWords.size()) {
+                    } else if (numberOfWordsThatHaveBeenFound == plaintextAsArrayOfWords.size()) {
                         return encryptedTextWithCompleteWord;
                     }
                 }
             }
+        }
+        catch (RuntimeException e){
+            System.err.println("There is something wrong with the programme, please try again or check your input");
+        }
             if (encryptedTextWithCompleteWord.isEmpty()){
                 throw new RuntimeException("Book doesn't contain one of the words that you have typed, or your word has been typed incorrectly");
             }
+
         return encryptedTextWithCompleteWord;
     }
