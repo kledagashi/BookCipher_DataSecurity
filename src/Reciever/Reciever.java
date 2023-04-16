45a6964811136c4bd7c55518f349e728d5f35314
KlajdiGashi <klajdi.gashi@student.uni-pr.edu>
2023-04-16 17:52:48 +0200

    Created  constructor Reciever

--- src/Reciever/Reciever.java
+++ src/Reciever/Reciever.java
@@ -3,4 +3,8 @@ package Reciever;
 class Reciever extends Person {
     public String plainTextIfCompleteWord;
     public String plainTextIfSingleLetter;
+
+    public Receiver(ArrayList<String> encryptedTextWithCompleteWord, ArrayList<String> encryptedTextWithSingleLetter) 
+    throws IOException {
+    }
 }
