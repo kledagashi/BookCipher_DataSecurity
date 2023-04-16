01faaf1ff4361e75bb5a5f31f68890d7f8625de3
Gentrit851 <53408143+Gentrit851@users.noreply.github.com>
2023-04-16 22:39:22 +0200

    Update generateEncryptedTextWithCompleteWord.java
    Updated the error handling method.

--- src/Sender/generateEncryptedTextWithCompleteWord.java
+++ src/Sender/generateEncryptedTextWithCompleteWord.java
@@ -1,12 +1,11 @@
-    public ArrayList<String> generateEncryptedTextWithCompleteWord(ArrayList<String> plaintextAsArrayOfWords, ArrayList<ArrayList<String>> bookAsArrayOfRows) {
+public ArrayList<String> generateEncryptedTextWithCompleteWord(ArrayList<String> plaintextAsArrayOfWords, ArrayList<ArrayList<String>> bookAsArrayOfRows) throws RuntimeException {
         ArrayList<String> encryptedTextWithCompleteWord = new ArrayList<>();
-        try {
             int numberOfWordsThatHaveBeenFound = 0;
             for (int i = 0; i < bookAsArrayOfRows.size(); i++) {
                 for (int j = 0; j < bookAsArrayOfRows.get(i).size(); j++) {
                     if (numberOfWordsThatHaveBeenFound < plaintextAsArrayOfWords.size()) {
                         if (bookAsArrayOfRows.get(i).get(j).equalsIgnoreCase(plaintextAsArrayOfWords.get(numberOfWordsThatHaveBeenFound))) {
-                            encryptedTextWithCompleteWord.add((i + 1) + "/" + (j + 1));
+                            encryptedTextWithCompleteWord.add((i + 1) + "/" + (j + 1) );
                             numberOfWordsThatHaveBeenFound++;
                             i = 0;
                             j = 0;
@@ -17,9 +16,8 @@
                     }
                 }
             }
-        }
-        catch (RuntimeException e) {
-            System.err.println("Book doesn't contain one of the words that you have typed, or your word has been typed incorrectly");
-        }
+            if (encryptedTextWithCompleteWord.isEmpty()){
+                throw new RuntimeException("Book doesn't contain one of the words that you have typed, or your word has been typed incorrectly");
+            }
         return encryptedTextWithCompleteWord;
     }
