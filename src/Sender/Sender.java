66e32b001c166f6f40f229f9579334fb419db784
Krenare158 <122061859+Krenare158@users.noreply.github.com>
2023-04-17 09:29:47 +1400

    Add files via upload

---
+++ src/Sender/Sender.java
@@ -0,0 +1,19 @@
+
+public class Sender {
+    public ArrayList<String> myEncryptedTextWithCompleteWords;
+    public ArrayList<String> myEncryptedTextWithSingleLetter;
+    private String myPlainText;
+    private ArrayList<String> plaintextAsArrayOfWords;
+    private ArrayList<Character> plaintextAsArrayOfCharacters;
+
+    public Sender(String myPlainText,String bookUrl) throws IOException {
+        super(bookUrl);
+        this.myPlainText = myPlainText;
+        this.plaintextAsArrayOfWords = this.turnSentenceInArrayOfWords(this.myPlainText);
+        this.plaintextAsArrayOfCharacters = this.turnSentenceInArrayOfCharacters(this.plaintextAsArrayOfWords);
+        this.myEncryptedTextWithCompleteWords = this.generateEncryptedTextWithCompleteWord(this.plaintextAsArrayOfWords, this.bookAsArrayOfRows);
+        this.myEncryptedTextWithSingleLetter = this.generateEncryptedTextWithSingleLetter(this.plaintextAsArrayOfCharacters, this.bookAsArrayOfRows);
+        System.out.println("Hello! This is my encrypted text with Complete Words: " + "\n " + this.myEncryptedTextWithCompleteWords);
+        System.out.println("Also if you need it here is it with Single Letter" + "\n " + this.myEncryptedTextWithSingleLetter);
+    }
+
